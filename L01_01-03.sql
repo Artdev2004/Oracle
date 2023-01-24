7ee27377d18f523f330ee35ca7260d436ee823cb
Arthur <arthur.soares.nascimento31@gmail.com>
2023-01-24 14:42:58 -0300

    Adicionando arquivo L01_04 drop tables e renomeando arquivos

--- L01_01-03.sql
+++ L01_01-03.sql
@@ -3,8 +3,10 @@ IDENTIFIED BY DESENV -- password desenv
 DEFAULT TABLESPACE USERS -- setting the default tablespace
 TEMPORARY TABLESPACE TEMP -- where stored temporary objects
 QUOTA UNLIMITED ON USERS; -- to create tables in users unlimitedly 
-
 alter session set "_ORACLE_SCRIPT"=TRUE;-- was necessary because the command, create user, wasn't working
+
+GRANT CONNECT, DBA TO DESENV; -- gave dba permissions
+
 
 
 
